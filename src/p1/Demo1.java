e033d231b354ef49dd48caeb5e3254821e2b3d25
mmendez9 <hikari1995>
2016-03-10 10:10:10 -0500

    Initial commit

---
+++ src/p1/Demo1.java
@@ -0,0 +1,24 @@
+package p1;
+
+/*
+ * Created by mmendez9 on 3/10/2016.
+ * CIS 150-401
+ * OK button
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Demo1 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button btOK = new Button("OK"); // Create a button
+        Scene scene = new Scene(btOK, 300, 300); // Create an scene and place the button in the scene
+        primaryStage.setTitle("Our first GUI"); // Set a title to the stage
+        primaryStage.setScene(scene); // Set an scene to the stage
+        primaryStage.show(); // Display the stage
+    }
+}
---
+++ src/p2/Demo2.java
@@ -0,0 +1,26 @@
+package p2;
+
+/*
+ * Created by mmendez9 on 3/10/2016.
+ * Very Ok button in a pane
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Demo2 extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane pane = new StackPane();
+        pane.getChildren().add(new Button("VERY \nVERY \nOK"));
+        pane.getChildren().add(new Button("VERY VERY OK"));
+        Scene scene = new Scene(pane, 200, 60);
+        primaryStage.setTitle("Button in Stack Pane");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
